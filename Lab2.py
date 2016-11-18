3166d4c3f91bc6965091473ee3871df4e4a68387
Joshua Galbraith <galbraithjt@gmail.com>
2016-11-18 10:58:53 -0800

    Comments Added

--- Lab2.py
+++ Lab2.py
@@ -1,4 +1,5 @@
-train = input ("Please choose a train enter the number coordinating with the destination: 1) New York City, NY 2) Buffalo, NY 3) Jersey City, NJ \n")
+train = input ("Please choose a train enter the number coordinating with the destination:\n"
+               " 1) New York City, NY 2) Buffalo, NY 3) Jersey City, NJ \n")
 
 train = int(train)
 
@@ -9,7 +10,14 @@ elif train == 2:
     cost = 5.25
 elif train == 3:
     cost = 7.50
+#system checks
+def checkSeatAvailability():#to Do
 
-print ("You have selected {} your cost is {} \nThere is a seat avalible on that train,\nPlease insert payment".format(train, cost))
+print ("You have selected {} your cost is {} \nThere is a seat available on that train,\n"
+       "Please insert payment".format(train, cost))
 
-print ("Thank you please take reciept below")
+#system connects to card company to verify payment
+def verifyPayment():#to DO
+
+#system prints receipt
+print ("Thank you please take receipt below")
